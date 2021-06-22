90a4fb3f03ecd3246a393b77cd463205b2c8dd4a
Chevap <luka.vuko@outlook.com>
2021-06-21 20:17:09 -0700

    removed more libraries

--- init.R
+++ init.R
@@ -4,22 +4,22 @@
 #
 
 # THIS METHOD TIMES OUT AND DOESNT WORK!!
-#my_packages = c("glue", "stringr", "DT", "ggpubr", "ggplot2", "dplyr", "tibble")
-#install_if_missing = function(p) {
-#  if (p %in% rownames(installed.packages()) == FALSE) {
-#    install.packages(p)
-#  }
-#}
-#invisible(sapply(my_packages, install_if_missing))
+my_packages = c("DT", "ggpubr", "ggplot2", "dplyr")
+
+install_if_missing = function(p) {
+  if (p %in% rownames(installed.packages()) == FALSE) {
+    install.packages(p)
+  }
+}
+invisible(sapply(my_packages, install_if_missing))
 
 # ALETERNATIVE METHOD FROM LOCAL
-install.packages("dplyr_1.0.7.tar.gz", repos=NULL, type="source")
-install.packages("DT_0.18.tar.gz", repos=NULL, type="source")
-install.packages("ggplot2_3.3.4.tar.gz", repos=NULL, type="source")
-install.packages("ggpubr_0.4.0.tar.gz", repos=NULL, type="source")
-install.packages("glue_1.4.2.tar.gz", repos=NULL, type="source")
-install.packages("stringr_1.4.0.tar.gz", repos=NULL, type="source")
-install.packages("tibble_3.1.2.tar.gz", repos=NULL, type="source")
+#install.packages("dplyr_1.0.7.tar.gz", repos=NULL, type="source")
+#install.packages("DT_0.18.tar.gz", repos=NULL, type="source")
+#install.packages("ggplot2_3.3.4.tar.gz", repos=NULL, type="source")
+#install.packages("ggpubr_0.4.0.tar.gz", repos=NULL, type="source")
+#install.packages("glue_1.4.2.tar.gz", repos=NULL, type="source")
+
 
 
 
